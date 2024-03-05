dfe401226ca853d61d1443f1ed886298edf10db4
Rahul Vishwakarma <rahul.vishwakarma@niser.ac.in>
2024-03-06 01:36:15 +0530

    Fix improper ideal case
    https://leanprover.zulipchat.com/#narrow/stream/219941-Machine-Learning-for-Theorem-Proving/topic/More.20papers.20on.20autoformalization/near/349258602

--- formal/Artin.lean
+++ formal/Artin.lean
@@ -89,7 +89,8 @@ theorem exercise_10_4_7a {R : Type*} [CommRing R] [NoZeroDivisors R]
 sorry
 
 theorem exercise_10_7_10 {R : Type*} [Ring R]
-  (M : Ideal R) (hM : ∀ (x : R), x ∉ M → IsUnit x) :
+  (M : Ideal R) (hM : ∀ (x : R), x ∉ M → IsUnit x)
+  (hProper : ∃ x : R, x ∉ M) :
   IsMaximal M ∧ ∀ (N : Ideal R), IsMaximal N → N = M :=
 sorry
 
