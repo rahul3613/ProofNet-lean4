4cf9fd20bf861b5db654d76235207befd72d16e5
ondanaoto <n.onda.go20@gmail.com>
2025-03-21 11:07:19 +0900

    version update of Putnam.lean

--- formal/Putnam.lean
+++ formal/Putnam.lean
@@ -41,7 +41,7 @@ theorem exercise_2010_a4 (n : ℕ) :
 sorry
 
 theorem exercise_2001_a5 :
-  ∃! a n : ℕ, a > 0 ∧ n > 0 ∧ a^(n+1) - (a+1)^n = 2001 :=
+  ∃! a : ℕ, ∃! n : ℕ, a > 0 ∧ n > 0 ∧ a^(n+1) - (a+1)^n = 2001 :=
 sorry
 
 theorem exercise_2000_a2 :
